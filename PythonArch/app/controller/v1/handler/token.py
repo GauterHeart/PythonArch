28d348580802dce84e817130633ef1d817f2bec4
GauterHeart <gauterheart@gmail.com>
2023-01-20 23:42:44 +0300

    refuck

--- PythonArch/app/controller/v1/handler/token.py
+++ PythonArch/app/controller/v1/handler/token.py
@@ -1,6 +1,5 @@
 from app.controller.v1.schema import token
 from app.crud import PostgresCRUD
-from app.pkg.exception import IdAlreadyExistException
 
 
 class TokenHandlerV1:
@@ -8,5 +7,4 @@ class TokenHandlerV1:
         self.__crud_p = crud_p
 
     async def create(self, spell: token.TokenCreateSchema) -> None:
-        raise IdAlreadyExistException()
-        # await self.__crud_p.token.create(token=spell.token)
+        await self.__crud_p.token.create(token=spell.token)
